2e4bc6d1df4435a0dd03373c713992c495844845
Mohamed Gibreel <mohamedgibreel@Mohameds-iMac-Pro.local>
2020-11-22 14:50:57 -0800

    Updates

---
+++ precache-manifest.4e279032ddc188d0eeb752fe2ec97a1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae27a2754454b8f0f52d9874cf9ee08a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33aabb80a56a7b44416a",
+    "url": "/static/css/main.b8ac88a5.chunk.css"
+  },
+  {
+    "revision": "293af0ebd0eea46c981f",
+    "url": "/static/js/2.bba2bd76.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.bba2bd76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6aa57e4431636262b7",
+    "url": "/static/js/3.67a6cedb.chunk.js"
+  },
+  {
+    "revision": "33aabb80a56a7b44416a",
+    "url": "/static/js/main.e41ab8ec.chunk.js"
+  },
+  {
+    "revision": "6c5f5cfc90f011812df5",
+    "url": "/static/js/runtime-main.58829822.js"
+  },
+  {
+    "revision": "a70b170b3d408f0fdc1cc9ea292e6fe2",
+    "url": "/static/media/appicon.a70b170b.svg"
+  }
+]);
